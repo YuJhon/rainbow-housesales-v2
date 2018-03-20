6b77dbf69419e755812757205da90764dc980faa
YuJhon <3291371805@qq.com>
2018-03-20 21:06:20 +0800

    Eureka注册中心搭建完成

--- house-eureka-server/src/main/java/com/jhon/rain/HouseEurekaServerApplication.java
+++ house-eureka-server/src/main/java/com/jhon/rain/HouseEurekaServerApplication.java
@@ -2,6 +2,7 @@ package com.jhon.rain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
